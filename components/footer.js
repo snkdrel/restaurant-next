917dff3553145c57b75a2200e9d9de46aa16d69a
snkdrel <ces.zop@gmail.com>
2023-06-02 16:52:59 -0600

    Change footer content

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,14 @@
+import Image from "next/image";
+
 export default function Footer() {
     return (
-        <div className="text-white text-center bg-dark p-3">Footer</div>
+        <div className="container-fluid bg-dark text-light">
+            <Image src="/images/location.png" alt="Ícono de ubicación" width={60} height={60}/>
+            <p>ubicacion</p>
+            <Image src="/images/phone-call.png" alt="Ícono de teléfono" width={60} height={60}/>
+            <p>telefono</p>
+            <Image src="/images/email.png" alt="Ícono de email" width={60} height={60}/>
+            <p>email</p>
+        </div>
     );
 }
