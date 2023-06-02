377a2be7645fc45fc2a84141fcc7f0a493519434
snkdrel <ces.zop@gmail.com>
2023-06-02 11:47:57 -0600

    Style navbar with bootstrap

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Menu from '../components/menu';
 import Footer from '../components/footer';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Menu />
-        <div>{children}</div>
-        <Footer />
+        <div>
+          <Menu />
+          <div>{children}</div>
+          <Footer />
+        </div>
       </body>
     </html>
   )
--- components/menu.js
+++ components/menu.js
@@ -2,25 +2,29 @@ import Link from "next/link";
 
 export default function Menu () {
     return (
-        <ul>
-            <li>
-                <Link href="/">Inicio</Link>
-            </li>
-            <li>
-                <Link href="/sobre-nosotros">Sobre nosotros</Link>
-            </li>
-            <li>
-                <Link href="/menu">Menú</Link>
-            </li>
-            <li>
-                <Link href="/reservaciones">Reservaciones</Link>
-            </li>
-            <li>
-                <Link href="/facturacion">Facturación</Link>
-            </li>
-            <li>
-                <Link href="/ubicacion">Ubicación</Link>
-            </li>
-        </ul>
+        <nav className="navbar  navbar-expand-sm">
+            <div className="container-fluid">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link href="/" className="nav-link">Inicio</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/sobre-nosotros" className="nav-link">Sobre nosotros</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/menu" className="nav-link">Menú</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/reservaciones" className="nav-link">Reservaciones</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/facturacion" className="nav-link">Facturación</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/ubicacion" className="nav-link">Ubicación</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
