dec368ee39618ac39ab4e41336d010db2f66263d
snkdrel <ces.zop@gmail.com>
2023-06-02 16:53:42 -0600

    Change forms for billing and reservation

--- app/facturacion/page.js
+++ app/facturacion/page.js
@@ -1,5 +1,29 @@
 export default function Billing() {
     return(
-        <div>Facturación</div>
+        <div className="container-fluid">
+            <form>
+                <div>
+                    <label className="form-label">RFC:
+                        <input type="text" className="form-control" id="rfc" name="rfc"></input>
+                    </label>
+                </div>
+                <div>
+                    <label className="form-label">Ticket ID:
+                        <input type="text" className="form-control" id="ticketid" name="ticketid"></input>
+                    </label>
+                </div>
+                <div>
+                    <label className="form-label">Monto:
+                        <input type="text" className="form-control" id="monto" name="monto"></input>
+                    </label>
+                </div>
+                <div>
+                    <label className="form-label">Soy Extranjero
+                        <input type="checkbox" className="form-check-input" id="extranjero" name="extranjero"></input>
+                    </label>
+                </div>
+                <button type="submit" className="btn btn-primary">Enviar</button>
+            </form>
+        </div>
     );
 }
--- app/reservaciones/page.js
+++ app/reservaciones/page.js
@@ -1,38 +1,59 @@
 export default function Reservations() {
     return(
-        <form action="#" method="post">
-            <fieldset>
-                <label>Fecha:
-                    <input type="date"></input>
-                </label>
-                <label>Personas:
-                    <select>
-                        <value>1 persona</value>
-                        <value>2 personas</value>
-                    </select>
-                </label>
-                <label>
-                    <select>
-                        <value>1</value>
-                        <value>2</value>
-                    </select>
-                </label>
-            </fieldset>
-            <fieldset>
-                <label>Nombre:
-                    <input type="text"></input>
-                </label>
-                <label>Apellidos:
-                    <input type="text"></input>
-                </label>
-                <label>Email:
-                    <input type="email"></input>
-                </label>
-                <label>Teléfono:
-                    <input type="tel"></input>
-                </label>
-            </fieldset>
-            <input type="submit" value="Reservar"></input>
-        </form>
+        <div className="container-fluid">
+            <form action="#" method="post">
+                <fieldset>
+                    <div>
+                        <label className="form-label">Fecha:
+                            <input type="date" className="form-control" id="date" name="date"></input>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="form-label">Personas:
+                            <input type="number" className="form-control" id="num-people" name="num-people" min={1} max={10}></input>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="form-label">Hora:
+                            <select id="time" name="time" className="form-select">
+                                <option value="12">12:00 pm</option>
+                                <option value="1">1:00 pm</option>
+                                <option value="2">2:00 pm</option>
+                                <option value="3">3:00 pm</option>
+                                <option value="4">4:00 pm</option>
+                                <option value="5">5:00 pm</option>
+                                <option value="6">6:00 pm</option>
+                                <option value="7">7:00 pm</option>
+                                <option value="8">8:00 pm</option>
+                                <option value="9">9:00 pm</option>
+                            </select>
+                        </label>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div>
+                        <label className="form-label">Nombre:
+                            <input type="text" className="form-control" id="nombre" name="nombre"></input>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="form-label">Apellidos:
+                            <input type="text" className="form-control" id="apellidos" name="apellidos"></input>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="form-label">Email:
+                            <input type="email" className="form-control" id="email" name="email"></input>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="form-label">Teléfono:
+                            <input type="tel" className="form-control" id="telefono" name="telefono"></input>
+                        </label>
+                    </div>
+                </fieldset>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
     );
 }
