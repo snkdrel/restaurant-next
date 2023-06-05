5ea57c15eb0ac0e93b5f572cdbb4ec12902d04c3
snkdrel <ces.zop@gmail.com>
2023-06-05 16:30:36 -0600

    Fix logo in homepage

--- components/logo.js
+++ components/logo.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Logo() {
     return (
         <div className="container text-center">
-            <div className="card" >
-                <Image className='card-img-top' src="/images/logo.jpeg" width={1200} height={300} alt="Logo"/>
+            <div className="card align-items-center" >
+                <Image className='card-img-top w-25 h-auto' src="/images/logo.png" alt="Logo" width={0} height={0} sizes="100vw"/>    
                 <div className="card-body">
                     <h4 className="card-title">Restaurante</h4>
                     <p className="card-test">Este es mi slogan</p>
